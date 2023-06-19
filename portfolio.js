abcbc561a756b96872944a1170069c18747bf0ec
AmJoy01 <amber.lewis01@gmail.com>
2023-06-19 19:29:12 -0400

    First commit

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+fetch("./navbar.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+fetch("./footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
